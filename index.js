fdedf2825e0434c480458cf3d9c49867d44b6c87
kcm0147 <kcm0147@hanmail.net>
2021-04-23 01:16:17 +0900

    block's status random

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var ctx = canvas.getContext("2d");
 var x = canvas.width / 2;
 var y = canvas.height - 30;
 var ballSize = 15;
-var dx = 3;
-var dy = -3;
+var dx = 0;
+var dy = -5;
 
 var lives = 1;
 
 //paddle
 var paddleHeight = 10;
-var paddleWidth = 75;
+var paddleWidth = 95;
 var paddleX = (canvas.width - paddleWidth) / 2;
 
 //paddle Control
@@ -20,8 +20,8 @@ var leftPressed = false;
 //wall
 
 var bricks = [];
-var brickRow = 3;
-var brickColumn = 5;
+var brickRow = 9;
+var brickColumn = 9;
 
 var brickWidth = 75;
 var brickHeight = 20;
@@ -29,7 +29,7 @@ var brickHeight = 20;
 var brickPadding = 10;
 
 var brickOffsetTop = canvas.height / 2 - 300;
-var brickOffsetLeft = canvas.width / 2 - 200;
+var brickOffsetLeft = canvas.width / 2 - 300;
 
 //score
 
@@ -39,11 +39,14 @@ var score = 0;
 
 function Init() {
 
+    var rand;
     //draw wall
     for (var c = 0; c < brickColumn; c++) {
         bricks[c] = [];
         for (var r = 0; r < brickRow; r++) {
-            bricks[c][r] = { x: 0, y: 0, status: 1 };
+            rand=Math.floor((Math.random()*3)+1);
+            console.log(rand);
+            bricks[c][r] = { x: 0, y: 0, status: rand };
         }
     }
 }
@@ -59,14 +62,21 @@ function drawWall() {
 
     for (var c = 0; c < brickColumn; c++) {
         for (var r = 0; r < brickRow; r++) {
-            if (bricks[c][r].status == 1) {
+            if (bricks[c][r].status == 1 || bricks[c][r].status==2 || bricks[c][r].status==3) {
                 var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
                 var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
                 bricks[c][r].x = brickX;
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
                 ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "#6F4F28";
+
+                if(bricks[c][r].status==1)
+                    ctx.fillStyle = "#6F4F28";
+                else if(bricks[c][r].status==2)
+                    ctx.fillStyle = "black";
+                else if(bricks[c][r].status==3)
+                    ctx.fillStyle = "grey";
+
                 ctx.fill();
                 ctx.closePath();
             }
@@ -80,10 +90,12 @@ function collisionWall() {
     for (var c = 0; c < brickColumn; c++) {
         for (var r = 0; r < brickRow; r++) {
             var b = bricks[c][r];
-            if (b.status == 1 && x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+            if (b.status > 0 && x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
                 dy = -dy;
-                b.status = 0;
-                score++;
+                --b.status;
+
+                if(b.status==0)
+                    score++;
 
                 if (score == brickColumn * brickHeight) {
                     alert("FINISH!!!");
@@ -121,8 +133,9 @@ function collision() {
         dy = -dy;
     }
     else if (y + dy > canvas.height - ballSize) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy;
+        if (x >= paddleX && x <= paddleX + paddleWidth) {
+            dx = -((paddleX + (paddleWidth/2)-x) / (paddleWidth))*10;
+            dy=-dy;
         }
         else {
             lives--;
